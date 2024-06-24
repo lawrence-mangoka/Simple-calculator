6d86263db3b3bc69b818daf719ad1d8dacc3d613
lawrence mangoka <164462342+lawrence-mangoka@users.noreply.github.com>
2024-06-24 10:42:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const display = document.getElementById("display")
+
+    function appendToDisplay(input){
+        display.value += input;
+    }
+
+    function clearDisplay() {
+        display.value = "";
+        
+    }
+
+    function appendToDelete() {
+      display.value = display.value.slice(0, -1)
+        
+    }
+
+
+
+    function calculate() {
+     try {
+        display.value = eval(display.value);
+     } catch (error) {
+        display.value = "error";
+     }   
+    }
+
